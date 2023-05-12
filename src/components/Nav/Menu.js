af60e56ffc8ee7d8fefa71887df0e893089b0c0a
Jyothish Sebastian <jyothish88@gmail.com>
2023-05-12 16:19:05 +0200

    update hamburger menu

--- src/components/Nav/Menu.js
+++ src/components/Nav/Menu.js
@@ -3,24 +3,21 @@ import { StyledMenu } from './Style';
 import { bool } from 'prop-types';
 
 const Menu = ({ open, ...props }) => {
-  
+
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
 
   return (
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
+
+      <a href="/" tabIndex={tabIndex}>
+        <span aria-hidden="true"></span>
+        ::.Lens Tale
+      </a>
       <a href="/stream" tabIndex={tabIndex}>
         <span aria-hidden="true"></span>
         Stream
       </a>
-      <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true"></span>
-        Stories
-        </a>
-      <a href="/" tabIndex={tabIndex}>
-        <span aria-hidden="true"></span>
-        Street
-        </a>
     </StyledMenu>
   )
 }
