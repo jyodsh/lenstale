d5d8052d61048e714df58629a9b4c1a045eb352d
Jyothish Sebastian <jyothish88@gmail.com>
2025-04-26 18:32:39 +0200

    Add descriptions to menu

--- src/components/Nav/Menu.js
+++ src/components/Nav/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StyledMenu } from './Style';
 import { bool } from 'prop-types';
+import styled from 'styled-components';
+
+const MenuDescription = styled.span`
+  font-size: 0.35em;
+  color: #888;
+  margin-top: 4px;
+  display: block;
+  width: 150px;
+  overflow-wrap: break-word;
+  text-transform: none;
+`;
 
 const Menu = ({ open, ...props }) => {
 
@@ -14,6 +25,9 @@ const Menu = ({ open, ...props }) => {
       </a>
       <a href="/stream" tabIndex={tabIndex}>
         Stream
+        <MenuDescription>
+          Selection of photos from around the world.
+        </MenuDescription>
       </a>
       <a href="/streets" tabIndex={tabIndex}>
         Streets
