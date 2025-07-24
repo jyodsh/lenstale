58309f9d907437053140ea18a5ce2993e987157a
Jyothish Sebastian <jyothish88@gmail.com>
2025-07-24 23:06:08 +0200

    update image import method

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,53 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { StaticImage } from "gatsby-plugin-image";
-import "../css/ImageSlider.css";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import "./imageSlider.css";
 
 const SLIDE_INTERVAL = 3000;
 
 const ImageSlider = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: { relativeDirectory: { eq: "slider" } }
+        sort: { name: ASC }
+      ) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+          }
+        }
+      }
+    }
+  `);
+
+  const images = data.allFile.nodes.map(node => getImage(node.childImageSharp));
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrent((prev) => (prev + 1) % 5);
+      setCurrent(prev => (prev + 1) % images.length);
     }, SLIDE_INTERVAL);
     return () => clearInterval(timer);
-  }, []);
+  }, [images.length]);
+
+  if (!images.length) return null;
 
   return (
     <div className="slider-container">
-      <StaticImage
-        src="../images/slider/slide1.jpg"
+      <GatsbyImage
+        image={images[current]}
         alt=""
-        className={`slider-image${current === 0 ? " active" : ""}`}
-        objectFit="cover"
-        objectPosition="center"
-      />
-      <StaticImage
-        src="../images/slider/slide2.jpg"
-        alt=""
-        className={`slider-image${current === 1 ? " active" : ""}`}
-        objectFit="cover"
-        objectPosition="center"
-      />
-      <StaticImage
-        src="../images/slider/slide3.jpg"
-        alt=""
-        className={`slider-image${current === 2 ? " active" : ""}`}
-        objectFit="cover"
-        objectPosition="center"
-      />
-      <StaticImage
-        src="../images/slider/slide4.jpg"
-        alt=""
-        className={`slider-image${current === 3 ? " active" : ""}`}
-        objectFit="cover"
-        objectPosition="center"
-      />
-      <StaticImage
-        src="../images/slider/slide5.jpg"
-        alt=""
-        className={`slider-image${current === 4 ? " active" : ""}`}
+        className="slider-image active"
         objectFit="cover"
         objectPosition="center"
       />
